9192b171c46dfd021c45e92c969c649e4dea2f32
michael walker <miwalker@atlassian.com>
2019-12-02 17:36:20 -0600

    changed some spacing to make me not loose my mind

--- resources/format_output.py
+++ resources/format_output.py
@@ -2,7 +2,7 @@ from colorama import Fore, Style
 
 class Formatter:
 	def format(disabled_bundled, raw_apps, env):
-		print("Plugin compatibility for ",env.product," on version ",env.version)
+		print("Plugin compatibility for",env.product,"on version",env.version)
 		not_compatible = []
 		not_latest = []
 		latest = []
@@ -21,22 +21,22 @@ class Formatter:
 			print("\nThe following Plugins are installed but NOT COMPATIBLE with your current version of ",
 				env.product, ": ",env.version)
 			for app in not_compatible:
-				print(Fore.RED,"  Name: ",app[2],"  Key: ",app[1],"  Current Version: ",app[3],Style.RESET_ALL)
+				print(Fore.RED,"  Name:",app[2],"   Key:",app[1],"   Current Version:",app[3],Style.RESET_ALL)
 		if len(not_latest) > 0:
 			print("\nThe following Plugins are COMPATIBLE with your current version of ",
 				env.product, " but are NOT running the latest version. ",
 				"Consider upgrading at your earliest convenience.")
 			for app in not_latest:
-				print(Fore.YELLOW,"  Name: ",app[2],"  Key: ",app[1],"  Current Version: ",app[3],Style.RESET_ALL)
+				print(Fore.YELLOW,"  Name:",app[2],"   Key:",app[1],"   Current Version:",app[3],Style.RESET_ALL)
 		if len(latest) > 0:
 			print("\nThe following Plugins are installed and Up to Date:")
 			for app in latest:
-				print(Fore.GREEN,"  Name: ",app[2],"  Key: ",app[1],"  Current Version: ",app[3],Style.RESET_ALL)
+				print(Fore.GREEN,"  Name:",app[2],"   Key:",app[1],"   Current Version:",app[3],Style.RESET_ALL)
 		if len(unknown) > 0:
 			print("\nThe following Plugins were not identified in the marketplace, ",
 				"meaning that they are likely in-house custom plugins, system plugins, or the version number could not be parsed successfully.")
 			for app in unknown:
-				print(Fore.WHITE,"  Name: ",app[2],"  Key: ",app[1],"  Current Version: ",app[3],Style.RESET_ALL)
+				print(Fore.WHITE,"  Name:",app[2],"   Key:",app[1],"   Current Version:",app[3],Style.RESET_ALL)
 		if len(disabled_bundled) > 0:			
 			print("The following are Apps that are apart of Bitbucket itself that are currently disabled.",
 				"\nWe Highly recommend re-enableing these apps.")
