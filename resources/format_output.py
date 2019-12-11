003fc9d3eb91672ea0724ce5097de877e424a11a
miwalker <miwalker@atlassian.com>
2019-12-10 18:27:58 -0600

    added line option for not compatible but latest release from vendor. Needs markdown equivalent still.

--- resources/format_output.py
+++ resources/format_output.py
@@ -3,21 +3,24 @@ from colorama import Fore, Style
 class Formatter:
 	def format(disabled_bundled, raw_apps, env, markdown):
 		not_compatible = []
+		not_compatible_latest = []
 		not_latest = []
 		latest = []
 		unknown = [] # Will likely consist of "Provided" status plugins
 		for app in raw_apps:
 			if app[0] == "unknown":
 				unknown.append(app)
-			elif app[0] == "incompatible":
+			elif app[0] == "incompatible" and app[5] == False:
 				not_compatible.append(app)
+			elif app[0] == "incompatible" and app[5] == True: #incompatible, but running the latest release possible meaning that upgrading isn't possible
+				not_compatible_latest.append(app)
 			elif app[0] == "compatible_old":
 				not_latest.append(app)
-			else:
+			else: # compatible and latest
 				latest.append(app)
 		# Need to add feature to mark if plugin is disabled or not. 
 		if markdown:
-			print('{panel:title=Plugin compatibility for',env.product,'on version',env.version,'}')
+			print('{panel:title=Plugin compatibility for'+env.product+'on version'+env.version+'}')
 			markdown_setup_line = "||Plugin Name||Plugin Key||Plugin Version||"
 			if len(not_compatible) > 0:
 				print("\nh3. The following Plugins are installed but NOT COMPATIBLE with your current version of ",
@@ -56,14 +59,18 @@ class Formatter:
 					env.product, ": ",env.version)
 				for app in not_compatible:
 					print(Fore.RED,"  Name:",app[2],"   Key:",app[1],"   Current Version:",app[3],Style.RESET_ALL)
+			if len(not_compatible_latest) > 0:
+				print("\nThe following Plugins are installed but NOT COMPATIBLE with your current version of "+
+					env.product + ": " + env.version + ", however, there are no newer releases. We recommend disabling these plugins and finding alternatives.")
+				for app in not_compatible_latest:
+					print(Fore.RED,"  Name:",app[2],"   Key:",app[1],"   Current Version:",app[3],Style.RESET_ALL)
 			if len(not_latest) > 0:
-				print("\nThe following Plugins are COMPATIBLE with your current version of ",
-					env.product, " but are NOT running the latest version. ",
-					"Consider upgrading at your earliest convenience.")
+				print("\nThe following Plugins are COMPATIBLE with your current version of " +
+					env.product + " but are NOT running the latest version. " + "Consider upgrading at your earliest convenience.")
 				for app in not_latest:
 					print(Fore.YELLOW,"  Name:",app[2],"   Key:",app[1],"   Current Version:",app[3],Style.RESET_ALL)
 			if len(latest) > 0:
-				print("\nThe following Plugins are installed and Up to Date:")
+				print("\nThe following Plugins are installed and up to date:")
 				for app in latest:
 					print(Fore.GREEN,"  Name:",app[2],"   Key:",app[1],"   Current Version:",app[3],Style.RESET_ALL)
 			if len(unknown) > 0:
