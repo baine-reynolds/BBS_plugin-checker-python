702215aef09d1427f341fab4c3595495ace7c7bc
baine-reynolds <baine.server@gmail.com>
2020-05-27 16:35:45 -0500

    fixed some output spacing

--- resources/format_output.py
+++ resources/format_output.py
@@ -24,7 +24,7 @@ class Formatter:
 				latest.append(app)
 		# Need to add feature to mark if plugin is disabled or not. 
 		if markdown:
-			print(f"{{panel:title=Plugin compatibility for {env.product} on version {env.version} }}")
+			print(f"{{panel:title=Plugin compatibility for {env.product} on version {env.version}}}")
 			markdown_setup_line = "||Plugin Name||Plugin Key||Plugin Version||"
 			if len(not_compatible) > 0:
 				print(f"\nh3. The following Plugins are installed but NOT COMPATIBLE with your current version of {env.product}: {env.version}")
@@ -32,14 +32,14 @@ class Formatter:
 				for app in not_compatible:
 					print(f"|{{color:red}}{app[2]}{{color}}|{{color:red}}{app[1]}{{color}}|{{color:red}}{app[3]}{{color}}|")
 			if len(not_compatible_latest) > 0:
-				print(f"\nh3. The following Plugins are installed but NOT COMPATIBLE with your current version of {env.product}: {env.version}, \
-					however, there are no newer releases. We recommend disabling these plugins and finding alternatives.")
+				print(f"\nh3. The following Plugins are installed but NOT COMPATIBLE with your current version of {env.product}: {env.version}," +
+					"however, there are no newer releases. We recommend disabling these plugins and finding alternatives.")
 				print(markdown_setup_line)
 				for app in not_compatible_latest:
 					print(f"|{{color:red}}{app[2]}{{color}}|{{color:red}}{app[1]}{{color}}|{{color:red}}{app[3]}{{color}}|")
 			if len(not_latest) > 0:
-				print(f"\nh3. The following Plugins are COMPATIBLE with your current version of {env.product} but are NOT running the latest version. \
-					\nConsider upgrading at your earliest convenience.")
+				print(f"\nh3. The following Plugins are COMPATIBLE with your current version of {env.product} but are NOT running the latest version." + 
+					" Consider upgrading at your earliest convenience.")
 				print(markdown_setup_line)
 				for app in not_latest:
 					print(f"|{{color:orange}}{app[2]}{{color}}|{{color:orange}}{app[1]}{{color}}|{{color:orange}}{app[3]}{{color}}|")
@@ -49,8 +49,8 @@ class Formatter:
 				for app in latest:
 					print(f"|{{color:green}}{app[2]}{{color}}|{{color:green}}{app[1]}{{color}}|{{color:green}}{app[3]}{{color}}|")
 			if len(unknown) > 0:
-				print(f"\nh3. The following Plugins were not identified in the marketplace, meaning that they are likely in-house custom plugins, \
-					system plugins, or the version number could not be parsed successfully.")
+				print(f"\nh3. The following Plugins were not identified in the marketplace, meaning that they are likely in-house custom plugins," +
+					" system plugins, or the version number could not be parsed successfully.")
 				print(markdown_setup_line)
 				for app in unknown:
 					print(f"|{{color:grey}}{app[2]}{{color}}|{{color:grey}}{app[1]}{{color}}|{{color:grey}}{app[3]}{{color}}|")
@@ -61,7 +61,7 @@ class Formatter:
 					print(f"|{{color:grey}}{app[2]}{{color}}|{{color:grey}}{app[1]}{{color}}|{{color:grey}}{app[3]}{{color}}|")
 			if len(disabled_bundled) > 0:			
 				print(f"\nh3. The following are Apps that are apart of {env.product} itself that are currently disabled." + 
-					"\nWe Highly recommend re-enableing these apps.")
+					" We Highly recommend re-enableing these apps.")
 				for app in disabled_bundled:
 					print("{color:red}" + app + "{color}")
 			print("{panel}")
@@ -72,13 +72,13 @@ class Formatter:
 				for app in not_compatible:
 					print(Fore.RED + f"  Name:{app[2]}   Key:{app[1]}   Current Version:{app[3]}" + Style.RESET_ALL)
 			if len(not_compatible_latest) > 0:
-				print(f"\nThe following Plugins are installed but NOT COMPATIBLE with your current version of {env.product}: {env.version}, however, \
-					there are no newer releases. We recommend disabling these plugins and finding alternatives.")
+				print(f"\nThe following Plugins are installed but NOT COMPATIBLE with your current version of {env.product}: {env.version}, however, there are no newer releases." +
+					" We recommend disabling these plugins and finding alternatives.")
 				for app in not_compatible_latest:
 					print(Fore.RED + f"  Name:{app[2]}   Key:{app[1]}   Current Version:{app[3]}" + Style.RESET_ALL)
 			if len(not_latest) > 0:
-				print(f"\nThe following Plugins are COMPATIBLE with your current version of {env.product} but are NOT running the latest version. \
-					Consider upgrading at your earliest convenience.")
+				print(f"\nThe following Plugins are COMPATIBLE with your current version of {env.product} but are NOT running the latest version." +
+					" Consider upgrading at your earliest convenience.")
 				for app in not_latest:
 					print(Fore.YELLOW + f"  Name:{app[2]}   Key:{app[1]}   Current Version:{app[3]}" + Style.RESET_ALL)
 			if len(latest) > 0:
@@ -86,8 +86,8 @@ class Formatter:
 				for app in latest:
 					print(Fore.GREEN + f"  Name:{app[2]}   Key:{app[1]}   Current Version:{app[3]}" + Style.RESET_ALL)
 			if len(unknown) > 0:
-				print(f"\nThe following Plugins were not identified in the marketplace, meaning that they are likely in-house custom plugins, \
-					system plugins, or the version number could not be parsed successfully.")
+				print(f"\nThe following Plugins were not identified in the marketplace, meaning that they are likely in-house custom plugins, " +
+					"system plugins, or the version number could not be parsed successfully.")
 				for app in unknown:
 					print(Fore.WHITE + f"  Name:{app[2]}   Key:{app[1]}   Current Version:{app[3]}" + Style.RESET_ALL)
 			if len(unknown_com_atl) > 0:
@@ -96,6 +96,6 @@ class Formatter:
 					print(Fore.WHITE + f"  Name:{app[2]}   Key:{app[1]}   Current Version:{app[3]}" + Style.RESET_ALL)
 			if len(disabled_bundled) > 0:			
 				print(f"\nThe following are Apps that are apart of {env.product} itself that are currently disabled." + 
-					"\nWe Highly recommend re-enableing these apps.")
+					" We Highly recommend re-enableing these apps.")
 				for app in disabled_bundled:
 					print(Fore.RED + app + Style.RESET_ALL)
